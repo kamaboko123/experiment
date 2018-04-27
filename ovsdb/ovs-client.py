f18604d4dec5249b0bf36ae4eb0e3a92608e5c61
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-04-27 19:40:11 +0900

    modify : ovs-client(test)

--- ovsdb/ovs-client.py
+++ ovsdb/ovs-client.py
@@ -6,7 +6,7 @@
 import socket
 import json
 import ipaddress
-import pprint
+from pprint import pprint
 
 RCV_BUF = 8192
 
@@ -117,9 +117,104 @@ class OvsdbClient:
                 ret[br["name"]]["ports"][port_uuid] = ports[port_uuid]
         
         return ret
-
+    
+    def get_interface(self):
+        query = {
+            "method":"transact",
+            "params":[
+                "Open_vSwitch",
+                {
+                    "op" : "select",
+                    "table" : "Interface",
+                    "where" : [],
+                }
+            ],
+            "id":0
+        }
+        
+        result = self._send(query)
+        
+        pprint(result["result"][0]["rows"])
+    
+    def add_interface(self):
+        query = {
+            "method":"transact",
+            "params":[
+                "Open_vSwitch",
+                {
+                    "uuid-name" : "test",
+                    "op" : "insert",
+                    "table" : "Interface",
+                    "row":{
+                        "name":"enp3s0",
+                        "type":""
+                    }
+                },
+                {
+                    "uuid-name": "test_p",
+                    "op" : "insert",
+                    "table" : "Port",
+                    "row":{
+                        "name" : "enp3s0",
+                        "interfaces":[
+                            "named-uuid",
+                            "test"
+                        ],
+                        "tag": 222
+                    }
+                },
+                {
+                    "where": [
+                        [
+                            "_uuid",
+                            "==",
+                            [
+                                "uuid",
+                                "a950561f-8068-45bb-a25a-83bcd8b3f944"
+                            ]
+                        ]
+                    ],
+                    "row": {
+                        "ports": [
+                            "set",
+                            [
+                                [
+                                    "named-uuid",
+                                    "test_p"
+                                ],
+                                [
+                                    "uuid",
+                                    "35243057-2ffd-4514-8d03-7b2b2cb05a8e"
+                                ],
+                                [
+                                    "uuid",
+                                    "60a408b3-5ac5-4ca6-ae94-d66b092b90e2"
+                                ],
+                                [
+                                    "uuid",
+                                    "c43fea7e-9b21-422d-a62f-41048eab1b48"
+                                ],
+                                [
+                                    "uuid",
+                                    "f53502df-5695-4a44-8b83-8f73d16d170d"
+                                ]
+                            ]
+                        ]
+                    },
+                    "op": "update",
+                    "table": "Bridge"
+                },
+                
+            ],
+            "id":100
+        }
+        
+        pprint(query)
+        result = self._send(query)
+        
+        pprint(result)
+    
 if __name__ == '__main__':
     ovsdb = OvsdbClient(5678)
     
-    print(json.dumps(ovsdb.get_bridge_config()))
-    
+    ovsdb.add_interface()
