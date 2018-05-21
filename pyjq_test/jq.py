0a95c3914e3ee1c0f4c38f2372e5a05cae99b7aa
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-05-21 22:17:14 +0900

    add : pyjq sample code

---
+++ pyjq_test/jq.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+import pyjq
+import json
+
+if __name__ == "__main__":
+    
+    json_data = None
+    
+    json_file = sys.argv[1]
+    with open(json_file, "r") as f:
+        json_data = json.loads(f.read())
+    
+    print json.dumps(pyjq.all('[.users[] | select (.data.age == 27) | .name]', json_data)[0])
+    
