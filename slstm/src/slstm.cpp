cdb0d732f5bb210e98c7369a9584075467340f25
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-08-07 22:44:17 +0900

    refactor : slstm

--- slstm/src/slstm.cpp
+++ slstm/src/slstm.cpp
@@ -6,48 +6,62 @@
 
 #define BUF_SIZE 256
 
+std::stack<OPERAND_TYPE> st;
+
 void dump_stack(std::stack<OPERAND_TYPE> st){
-    printf("----[Stack]----\n");
-    for(int i = 0; st.size() > 0; i++){
-        fprintf(stderr, "%2x: %2x\n", i, st.top());
+    fprintf(stderr, "|Stack|");
+    while(st.size() > 0){
+        fprintf(stderr, " 0x%.2x", st.top());
         st.pop();
     }
-    printf("---------------\n\n");
+    fprintf(stderr, "\n");
+}
+
+void push(OPERAND_TYPE n){
+    st.push(n);
+}
+
+OPERAND_TYPE pop(){
+    int n = st.top();
+    st.pop();
+    
+    return n;
+}
+
+void add(){
+    int n1 = pop();
+    int n2 = pop();
+    st.push(n1 + n2);
+}
+
+void sub(){
+    int n1 = pop();
+    int n2 = pop();
+    st.push(n2 - n1);
 }
 
 int main(void){
     fprintf(stderr, "word length : %ld\n", sizeof(Word));
+    
     uint8_t buf[BUF_SIZE] = {0};
     read(0, buf, BUF_SIZE);
-    
-    std::stack<OPERAND_TYPE> st;
     
     Word *p = (Word *)buf;
     
     dump_stack(st);
     for(int i = 0; p->op != INS_END; i += sizeof(Word)){
         switch(p->op){
-            OPERAND_TYPE n1, n2;
-            
             case INS_PUSH:
-                st.push(p->arg);
+                push(p->arg);
                 break;
             case INS_POP:
-                st.pop();
+                pop();
                 break;
             case INS_ADD:
-                n1 = st.top();
-                st.pop();
-                n2 = st.top();
-                st.pop();
-                st.push(n1 + n2);
+                add();
                 break;
             case INS_SUB:
-                n1 = st.top();
-                st.pop();
-                n2 = st.top();
-                st.pop();
-                st.push(n2 - n1);
+                sub();
                 break;
             default:
                 break;
